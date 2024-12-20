852215222b3d3217e97a86e1f06078da48e3696b
DopeGhoti <dopeghoti+github@gmail.com>
2024-12-19 19:20:52 -0700

    Tabs to spaces; four spaces per indent

--- allslain.py
+++ allslain.py
@@ -10,94 +10,96 @@ LOG_RESPAWN = re.compile(r"<(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}).\d{3}Z> \[Noti
 
 
 def follow(f):
-	while True:
-		if line := f.readline():
-			yield line
-		else:
-			time.sleep(1)
-			continue
+    while True:
+        if line := f.readline():
+            yield line
+        else:
+            time.sleep(1)
+            continue
 
 
 def clean_location(name):
-	name_split = name.split("_")
-	if name[0] == "RastarInteriorGridHost":
-		return name[0]
-	short_name = []
-	for i in name_split:
-		try:
-			int(i)
-		except:
-			short_name.append(i)
-	return "_".join(short_name)
+    name_split = name.split("_")
+    if name[0] == "RastarInteriorGridHost":
+        return name[0]
+    short_name = []
+    for i in name_split:
+        try:
+            int(i)
+        except:
+            short_name.append(i)
+    return "_".join(short_name)
 
 
 def clean_name(name):
-	if name.startswith("PU_Human_Enemy"):
-		name_split = name.split("_")
-		return "_".join(["NPC", *name_split[5:6]])
-	if name.startswith("PU_Human-"):
-		name_split = name.split("-")
-		return "_".join(["NPC", *name_split[1:3]])
-	if name.startswith("NPC_Archetypes-"):
-		name_split = name.split("-")
-		return "_".join(["NPC", *name_split[3].split("_")[0:1], name_split[1]])
-	if name.startswith("Kopion_"):
-		return "_".join(["NPC", "Kopion"])
-	return name
+    if name.startswith("PU_Human_Enemy"):
+        name_split = name.split("_")
+        return "_".join(["NPC", *name_split[5:6]])
+    if name.startswith("PU_Human-"):
+        name_split = name.split("-")
+        return "_".join(["NPC", *name_split[1:3]])
+    if name.startswith("NPC_Archetypes-"):
+        name_split = name.split("-")
+        return "_".join(["NPC", *name_split[3].split("_")[0:1], name_split[1]])
+    if name.startswith("Kopion_"):
+        return "_".join(["NPC", "Kopion"])
+    return name
 
 
 def clean_tool(name):
-	if name == 'Player':
-		return 'suicide'
-	if name == "unknown":
-		return name
-	try:
-		name_split = name.split("_")
-		return "_".join(name_split[0:1])
-	except IndexError:
-		return name
+    if name == 'Player':
+        return 'suicide'
+    if name == "unknown":
+        return name
+    try:
+        name_split = name.split("_")
+        return "_".join(name_split[0:1])
+    except IndexError:
+        return name
 
 
 def main(filepath, show_npc_victims):
-	KILL=f'{C.FG("RED", bold = True )}{"KILL":>10}{C.reset()}'
-	VKILL=f'{C.FG("RED", bold = True )}{"VKILL":>10}{C.reset()}'
-	RESPAWN=f'{C.FG("CYAN", bold = True )}{"RESPAWN":>10}{C.reset()}'
+    KILL=f'{C.FG("RED", bold = True )}{"KILL":>10}{C.reset()}'
+    VKILL=f'{C.FG("RED", bold = True )}{"VKILL":>10}{C.reset()}'
+    RESPAWN=f'{C.FG("CYAN", bold = True )}{"RESPAWN":>10}{C.reset()}'
 
-	try:
-		f = open(filepath, "r")
-		for line in follow(f):
-			m = LOG_KILL.match(line)
-			if m:
-				# datetime, killed, location, killer, killed_using
-				killed = clean_name(m[2])
-				if "NPC" in killed:
-					if show_npc_victims:
-						print( f'{m[1]}{KILL}: {clean_name(m[4])} {clean_tool(m[5])} {killed} at {clean_location(m[3])}' )
-						continue
-					else:
-						continue
-				else:
-					print( f'{m[1]}{KILL}: {clean_name(m[4])} {clean_tool(m[5])} {killed} at {clean_location(m[3])}' )
-					continue
-			n = LOG_VEHICLE_KILL.match(line)
-			if n:
-				# datetime, vehicle, location, driver, caused_by, damage_type
-				print( f'{n[1]}{VKILL}: {clean_location(n[3])} {n[4]} {clean_location(n[2])} {n[5]} {n[6]}' )
-				continue
-			o = LOG_RESPAWN.match(line)
-			if o:
-				# datetime, player, location
-				print( f'{o[1]}{RESPAWN}{C.reset()}: {o[2]} {o[3]}' )
-				continue
-	except KeyboardInterrupt:
-		f.close()
-	except FileNotFoundError:
-		print(f'{C.FG("RED", bold = True)}Log file {filename} not found.{C.reset()}\nRun this again from within the game folder after starting the game, or specify a game log to read.')
+    try:
+        f = open(filepath, "r")
+        for line in follow(f):
+            m = LOG_KILL.match(line)
+            if m:
+                # datetime, killed, location, killer, killed_using
+                killed = clean_name(m[2])
+                if "NPC" in killed:
+                    if show_npc_victims:
+                        print( f'{m[1]}{KILL}: {clean_name(m[4])} {clean_tool(m[5])} {killed} at {clean_location(m[3])}' )
+                        continue
+                    else:
+                        continue
+                else:
+                    print( f'{m[1]}{KILL}: {clean_name(m[4])} {clean_tool(m[5])} {killed} at {clean_location(m[3])}' )
+                    continue
+            n = LOG_VEHICLE_KILL.match(line)
+            if n:
+                # datetime, vehicle, location, driver, caused_by, damage_type
+                print( f'{n[1]}{VKILL}: {clean_location(n[3])} {n[4]} {clean_location(n[2])} {n[5]} {n[6]}' )
+                continue
+            o = LOG_RESPAWN.match(line)
+            if o:
+                # datetime, player, location
+                print( f'{o[1]}{RESPAWN}{C.reset()}: {o[2]} {o[3]}' )
+                continue
+    except KeyboardInterrupt:
+        f.close()
+    except FileNotFoundError:
+        print(f'{C.FG("RED", bold = True)}Log file {filename} not found.{C.reset()}\nRun this again from within the game folder after starting the game, or specify a game log to read.')
 
 
 if __name__ == '__main__':
-	filename = "Game.log"
-	if len(sys.argv) >= 2:
-		filename = sys.argv[1]
-	show_npc_victims = True
-	main(filename, show_npc_victims)
+    filename = "Game.log"
+    if len(sys.argv) >= 2:
+        filename = sys.argv[1]
+    show_npc_victims = True
+    main(filename, show_npc_victims)
+
+# vim: set expandtab ts=4 sw=4
--- colorize.py
+++ colorize.py
@@ -4,32 +4,32 @@
 ESC = '\033'
 
 COLORS= {
-      'BLACK':   0,
-      'RED':     1, 
-      'GREEN':   2,
-      'YELLOW':  3,
-      'BLUE':    4,
-      'PURPLE':  5,
-      'CYAN':    6,
-      'WHITE':   7
-      }
+        'BLACK':   0,
+        'RED':     1, 
+        'GREEN':   2,
+        'YELLOW':  3,
+        'BLUE':    4,
+        'PURPLE':  5,
+        'CYAN':    6,
+        'WHITE':   7
+        }
 
 def FG( color, bold = False ):
-   if bold:
-      BOLD="1;"
-   else:
-      BOLD = ""
-   return f'{ESC}[3{COLORS[color]}m'
+    if bold:
+        BOLD="1;"
+    else:
+        BOLD = ""
+    return f'{ESC}[3{COLORS[color]}m'
 
 def BG( color, bold = False ):
-   if bold:
-      BOLD="1;"
-   else:
-      BOLD = ""
-   return f'{ESC}[4{COLORS[color]}m'
+    if bold:
+        BOLD="1;"
+    else:
+        BOLD = ""
+    return f'{ESC}[4{COLORS[color]}m'
 
 def reset():
-   return f'{BG("BLACK")}{FG("WHITE")}'
+    return f'{BG("BLACK")}{FG("WHITE")}'
 
 
-
+# vim: set expandtab ts=4 sw=4
