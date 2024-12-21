9e52d3f79c76a15e192d35fcb58d518ab8c6c68e
Dimma Don't <93484965+DimmaDont@users.noreply.github.com>
2024-12-21 17:42:34 -0500

    reformat

--- allslain.py
+++ allslain.py
@@ -7,9 +7,15 @@ from colorize import Color
 from data import SHIPS, WEAPONS_FPS, WEAPONS_SHIP
 
 
-LOG_KILL = re.compile(r"<(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}).\d{3}Z> \[Notice\] <Actor Death> CActor::Kill: '([A-Za-z0-9_-]+)' \[\d+\] in zone '([A-Za-z0-9_-]+)' killed by '([A-Za-z0-9_-]+)' \[\d+\] using '[A-Za-z0-9_-]+' \[Class ([A-Za-z0-9_-]+)\] with damage type '([A-Za-z]+)' from direction (.*) \[Team_ActorTech\]\[Actor\]")
-LOG_VEHICLE_KILL = re.compile(r"<(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}).\d{3}Z> \[Notice\] <Vehicle Destruction> CVehicle::OnAdvanceDestroyLevel: Vehicle '([A-Za-z0-9_-]+)' \[\d+\] in zone '([A-Za-z0-9_-]+)' \[pos.*\] driven by '([A-Za-z0-9_-]+)' \[\d+\] advanced from destroy level \d to (\d) caused by '([A-Za-z0-9_-]+)' \[[0-9_]+\] with '([A-Za-z]+)' \[Team_VehicleFeatures\]\[Vehicle\]")
-LOG_RESPAWN = re.compile(r"<(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}).\d{3}Z> \[Notice\] <Corpse> Player '([A-Za-z0-9_-]+)' <(?:remote|local) client>: DoesLocationContainHospital: Searching landing zone location \"(.*)\" for the closest hospital. \[Team_ActorTech\]\[Actor\]")
+LOG_KILL = re.compile(
+    r"<(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}).\d{3}Z> \[Notice\] <Actor Death> CActor::Kill: '([A-Za-z0-9_-]+)' \[\d+\] in zone '([A-Za-z0-9_-]+)' killed by '([A-Za-z0-9_-]+)' \[\d+\] using '[A-Za-z0-9_-]+' \[Class ([A-Za-z0-9_-]+)\] with damage type '([A-Za-z]+)' from direction (.*) \[Team_ActorTech\]\[Actor\]"
+)
+LOG_VEHICLE_KILL = re.compile(
+    r"<(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}).\d{3}Z> \[Notice\] <Vehicle Destruction> CVehicle::OnAdvanceDestroyLevel: Vehicle '([A-Za-z0-9_-]+)' \[\d+\] in zone '([A-Za-z0-9_-]+)' \[pos.*\] driven by '([A-Za-z0-9_-]+)' \[\d+\] advanced from destroy level \d to (\d) caused by '([A-Za-z0-9_-]+)' \[[0-9_]+\] with '([A-Za-z]+)' \[Team_VehicleFeatures\]\[Vehicle\]"
+)
+LOG_RESPAWN = re.compile(
+    r"<(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}).\d{3}Z> \[Notice\] <Corpse> Player '([A-Za-z0-9_-]+)' <(?:remote|local) client>: DoesLocationContainHospital: Searching landing zone location \"(.*)\" for the closest hospital. \[Team_ActorTech\]\[Actor\]"
+)
 
 
 def follow(f):
@@ -26,8 +32,8 @@ def clean_location(name):
     if name[0] == "RastarInteriorGridHost":
         return name[0]
     short_name = []
-    if 'ugf' in name.lower():
-        return "Bunker" # UGF is CIG-ese for what most folks call "Bunkers"
+    if "ugf" in name.lower():
+        return "Bunker"  # UGF is CIG-ese for what most folks call "Bunkers"
     for i in name_split:
         try:
             int(i)
@@ -58,8 +64,8 @@ def clean_name(name):
 
 
 def clean_tool(name: str, killer: str, killed: str) -> str:
-    if name == 'Player':
-        return 'suicide'
+    if name == "Player":
+        return "suicide"
 
     if name == "unknown":
         # Ship collision
@@ -84,7 +90,7 @@ def get_vehicle(name) -> str:
     # Remove vehicle id
     try:
         i = name.rindex("_")
-        vehicle_id = name[i + 1:]
+        vehicle_id = name[i + 1 :]
         int(vehicle_id)
         # has vehicle id
         name = name[:i]
@@ -107,7 +113,7 @@ RESPAWN = Color.CYAN("RESPAWN".rjust(10))
 def main(filepath, show_npc_victims):
     try:
         f = open(filepath, "r")
-        print(Color.RESET, end='')
+        print(Color.RESET, end="")
         for line in follow(f):
             if m := LOG_KILL.match(line):
                 # datetime, killed, location, killer, killed_using
@@ -118,10 +124,14 @@ def main(filepath, show_npc_victims):
                 cause = clean_tool(m[5], killer, killed)
                 if "NPC" in killed and not show_npc_victims:
                     continue
-                elif cause == 'suicide':
-                    print( f'{when}{KILL}: {Color.GREEN(killer)} committed {Color.CYAN(cause)} at {Color.YELLOW(location)}' )
+                elif cause == "suicide":
+                    print(
+                        f"{when}{KILL}: {Color.GREEN(killer)} committed {Color.CYAN(cause)} at {Color.YELLOW(location)}"
+                    )
                 else:
-                    print( f'{when}{KILL}: {Color.GREEN(killer)} killed {Color.GREEN(killed)} with a {Color.CYAN(cause)} at {Color.YELLOW(location)}' )
+                    print(
+                        f"{when}{KILL}: {Color.GREEN(killer)} killed {Color.GREEN(killed)} with a {Color.CYAN(cause)} at {Color.YELLOW(location)}"
+                    )
                 continue
             if n := LOG_VEHICLE_KILL.match(line):
                 # datetime, vehicle, location, driver, caused_by, damage_type
@@ -129,14 +139,16 @@ def main(filepath, show_npc_victims):
                 vehicle = Color.GREEN(get_vehicle(n[2]))
                 location = Color.YELLOW(clean_location(n[3]))
                 driver = clean_name(n[4])
-                if driver == 'unknown':
-                    driver = ''
+                if driver == "unknown":
+                    driver = ""
                 else:
                     driver = Color.GREEN(driver) + " in a "
                 kill_type = n[5]
                 cause = Color.GREEN(get_vehicle(n[6]))
                 dmgtype = Color.CYAN(n[7])
-                print( f'{when}{VKILL}: {cause} {"soft" if kill_type == '1' else "hard"} killed {driver}{vehicle} with {dmgtype} at {location}' )
+                print(
+                    f'{when}{VKILL}: {cause} {"soft" if kill_type == '1' else "hard"} killed {driver}{vehicle} with {dmgtype} at {location}'
+                )
                 continue
             o = LOG_RESPAWN.match(line)
             if o:
@@ -144,16 +156,18 @@ def main(filepath, show_npc_victims):
                 when = o[1]
                 whom = Color.GREEN(o[2])
                 where = Color.YELLOW(o[3])
-                print(f'{o[1]}{RESPAWN}: {whom} at? {where}')
+                print(f"{o[1]}{RESPAWN}: {whom} at? {where}")
                 continue
     except KeyboardInterrupt:
         f.close()
     except FileNotFoundError:
         print(Color.RED(f"Log file {filepath} not found."))
-        print("Run this again from within the game folder after starting the game, or specify a game log to read.")
+        print(
+            "Run this again from within the game folder after starting the game, or specify a game log to read."
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     filename = "Game.log"
     if len(sys.argv) >= 2:
         filename = sys.argv[1]
