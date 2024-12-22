31e18ea5eab729b60487af8ccfbbf8a0e85c5feb
Dimma Don't <93484965+DimmaDont@users.noreply.github.com>
2024-12-21 19:12:06 -0500

    add tests

---
+++ test_colorize.py
@@ -0,0 +1,14 @@
+import unittest
+
+from colorize import *
+
+
+class TestNameFunctions(unittest.TestCase):
+
+    def test_fg(self):
+        result = f"{Color.BLUE("hello")} there"
+        self.assertEqual(result, f"\x1b[34mhello\x1b[0m there")
+
+
+if __name__ == "__main__":
+    unittest.main()
