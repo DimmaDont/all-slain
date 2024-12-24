d8d3373e71be3eba64b0332e2751d5546030ac3d
DopeGhoti <dopeghoti+github@gmail.com>
2024-12-23 17:24:55 -0700

    Fix issue with quotes

--- test_colorize.py
+++ test_colorize.py
@@ -7,7 +7,7 @@ from colorize import *
 class TestNameFunctions(unittest.TestCase):
 
     def test_fg(self):
-        result = f"{Color.BLUE("hello")} there"
+        result = f"{Color.BLUE('hello')} there"
         self.assertEqual(result, f"\x1b[34mhello\x1b[0m there")
 
 
