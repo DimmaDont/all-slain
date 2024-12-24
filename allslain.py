0a75cb3e731056ff6df9879812b94f58e20918b5
DopeGhoti <dopeghoti+github@gmail.com>
2024-12-24 15:14:34 -0700

    Standardize naming in log parsing routines

--- allslain.py
+++ allslain.py
@@ -176,12 +176,12 @@ def main(filepath: str) -> None:
                 "incap": LOG_INCAP.match(line),
             }
             if any(matches):
-                if matches["pkill"]:
-                    when = match_kill[1].replace("T", " ")
-                    killed, is_killed_npc = clean_name(match_kill[2])
-                    lp, location = clean_location(match_kill[3])
-                    killer, is_killer_npc = clean_name(match_kill[4])
-                    cause = clean_tool(match_kill[5], killer, killed)
+                if log := matches["pkill"]:
+                    when = log[1].replace("T", " ")
+                    killed, is_killed_npc = clean_name(log[2])
+                    lp, location = clean_location(log[3])
+                    killer, is_killer_npc = clean_name(log[4])
+                    cause = clean_tool(log[5], killer, killed)
                     if is_killer_npc and is_killed_npc:
                         print(
                             f"{when}{KILL}: {Color.BLACK(killer, True)} killed {Color.BLACK(killed, True)} with a {Color.CYAN(cause)} {lp} {Color.YELLOW(location)}"
@@ -194,33 +194,33 @@ def main(filepath: str) -> None:
                         print(
                             f"{when}{KILL}: {Color.GREEN(killer)} killed {Color.GREEN(killed)} with a {Color.CYAN(cause)} {lp} {Color.YELLOW(location)}"
                         )
-                elif matches["vkill"]:
-                    when = match_vkill[1].replace("T", " ")
+                elif log := matches['vkill']:
+                    when = log[1].replace("T", " ")
                     # note: vehicle can also be an npc/player entity if it's a collision
-                    vehicle = Color.GREEN(get_vehicle(match_vkill[2]))
-                    lp, location = clean_location(match_vkill[3])
-                    driver, _ = clean_name(match_vkill[4])
+                    vehicle = Color.GREEN(get_vehicle(log[2]))
+                    lp, location = clean_location(log[3])
+                    driver, _ = clean_name(log[4])
                     if driver == "unknown":
                         driver = ""
                     else:
                         driver = Color.GREEN(driver) + " in a "
-                    kill_type = match_vkill[5]
-                    killer = Color.GREEN(get_vehicle(match_vkill[6]))
-                    dmgtype = Color.CYAN(match_vkill[7])
+                    kill_type = log[5]
+                    killer = Color.GREEN(get_vehicle(log[6]))
+                    dmgtype = Color.CYAN(log[7])
                     print(
                         f'{when}{VKILL}: {killer} {Color.YELLOW("disabled") if kill_type == "1" else Color.RED("destroyed")} a {driver}{vehicle} with {dmgtype} {lp} {Color.YELLOW(location)}'
                     )
-                elif matches["spawn"]:
+                elif log := matches['spawn']:
                     # datetime, player, location
-                    when = match_respawn[1].replace("T", " ")
-                    whom = Color.GREEN(match_respawn[2])
-                    lp, where = clean_location(match_respawn[3])
+                    when = log[1].replace("T", " ")
+                    whom = Color.GREEN(log[2])
+                    lp, where = clean_location(log[3])
                     print(f"{when}{RESPAWN}: {whom} {lp} {Color.YELLOW(where)}")
-                elif matches["incap"]:
+                elif log := matches['incap']:
                     # datetime, player, location
-                    when = match_incap[1].replace("T", " ")
-                    whom = Color.GREEN(match_incap[2])
-                    causes = LOG_INCAP_CAUSE.findall(match_incap[3])
+                    when = log[1].replace("T", " ")
+                    whom = Color.GREEN(log[2])
+                    causes = LOG_INCAP_CAUSE.findall(log[3])
                     print(
                         f"{when}{INCAP}: {whom} from {', '.join([Color.YELLOW(cause[0].replace('Damage', '')) for cause in causes])}"
                     )
