4baecb5d9500ff174ae5cc5e1216561455aaab11
DopeGhoti <dopeghoti+github@gmail.com>
2024-12-26 11:23:21 -0700

    New features for colorize
    Allow setting color without resetting to default
    Abstract out the color constant from the bg/bg/bold modifiers

--- colorize.py
+++ colorize.py
@@ -3,18 +3,26 @@ from enum import IntEnum
 
 
 class Color(IntEnum):
-    BLACK = 30
-    RED = 31
-    GREEN = 32
-    YELLOW = 33
-    BLUE = 34
-    MAGENTA = 35
-    CYAN = 36
-    WHITE = 37
+    BLACK = 0
+    RED = 1
+    GREEN = 2
+    YELLOW = 3
+    BLUE = 4
+    MAGENTA = 5
+    CYAN = 6
+    WHITE = 7
 
-    def __call__(self, text: str, bold: bool = False, bg: bool = False) -> str:
-        color_code = self.value + (60 if bold else 0) + (10 if bg else 0)
+    def __call__(self, text: str, fg: bool = True, bold: bool = False, bg: bool = False) -> str:
+        color_code = self.value + (30 if fg else 0) + (60 if bold else 0) + (40 if bg else 0)
         return f"\x1b[{color_code}m{text}\x1b[0m"
+
+    def set( self, fg: bool = False, bg: bool = False, bold: bool = False ) -> str:
+        color_code = self.value + (30 if fg else 0) + (60 if bold else 0) + (40 if bg else 0)
+        return f"\x1b[{color_code}m"
+
+    @staticmethod
+    def reset() -> str:
+        return f"\x1b[0m"
 
     @staticmethod
     def rgb(r: int, g: int, b: int, text: str, bg: bool = False) -> str:
--- test_colorize.py
+++ test_colorize.py
@@ -11,15 +11,15 @@ class TestNameFunctions(unittest.TestCase):
         self.assertEqual(result, "\x1b[34mhello\x1b[0m there")
 
     def test_fg_bold(self):
-        result = f"{Color.BLUE('hello', True)} there"
+        result = f"{Color.BLUE('hello', bold = True)} there"
         self.assertEqual(result, "\x1b[94mhello\x1b[0m there")
 
     def test_bg(self):
-        result = f"{Color.BLUE('hello', False, True)} there"
+        result = f"{Color.BLUE('hello', fg = False, bold = False, bg = True)} there"
         self.assertEqual(result, "\x1b[44mhello\x1b[0m there")
 
     def test_bg_bold(self):
-        result = f"{Color.BLUE('hello', True, True)} there"
+        result = f"{Color.BLUE('hello', fg = False, bold = True, bg = True)} there"
         self.assertEqual(result, "\x1b[104mhello\x1b[0m there")
 
     def test_rgb(self):
@@ -27,8 +27,12 @@ class TestNameFunctions(unittest.TestCase):
         self.assertEqual(result, "\x1b[38;2;1;2;3mhello\x1b[0m there")
 
     def test_rgb_bold(self):
-        result = f"{Color.rgb(1, 2, 3, 'hello', True)} there"
+        result = f"{Color.rgb(1, 2, 3, 'hello', bg = True)} there"
         self.assertEqual(result, "\x1b[48;2;1;2;3mhello\x1b[0m there")
+
+    def test_inline(self):
+        result = f'{Color.BLUE.set(fg = True)}hello{Color.reset()}'
+        self.assertEqual( result, Color.BLUE('hello') )
 
 
 if __name__ == "__main__":
