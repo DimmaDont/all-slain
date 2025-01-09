fa39584cc1d4eeeefb00b2744e098e20dee6dd47
DopeGhoti <dopeghoti+github@gmail.com>
2025-01-08 21:37:48 -0700

    New f-string-like parser for Color, parse()
    Sprinkle markers like (R) (for "Red") or (Bb) (for
    "Blue background") or "(X)" for "Reset" into your
    strings, and run them through Color.parse() to get
    color coding injected into the string, e. g.:
    
    s = 'This should be (R)red(X).'
    print( Color.parse( s ) )

--- test_colorize.py
+++ test_colorize.py
@@ -38,6 +38,21 @@ class TestNameFunctions(unittest.TestCase):
         result = f'{Color.BLUE("hello")} there'
         self.assertEqual(result, "\x1b[34mhello\x1b[0m there")
 
+    def test_parse(self):
+        test_strings = [
+                Color.parse( 'This is a test') ,
+                Color.parse( 'This is a (Rf)test(X).') ,
+                Color.parse( 'This is a (R)test.(X)') ,
+                Color.parse( 'This is a (Rf)(Bb)test(X).') ,
+                ]
+        valid_strings = [
+                f'This is a test',
+                f'This is a {Color.RED("test")}.',
+                f'This is a {Color.RED("test.")}',
+                f'This is a {Color.RED.set()}{Color.BLUE.set( bg = True, fg = False )}test{Color.reset()}.',
+                ]
+        for parse_str, valid_str in zip( test_strings, valid_strings ):
+            self.assertEqual( parse_str, valid_str )
 
 if __name__ == "__main__":
     unittest.main()
