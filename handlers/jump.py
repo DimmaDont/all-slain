40e13ed12ca72b44d6efab2ea13bfa60ecf8e8ac
Dimma Don't <93484965+DimmaDont@users.noreply.github.com>
2025-02-26 22:54:00 -0500

    clarify jump message

--- handlers/jump.py
+++ handlers/jump.py
@@ -8,11 +8,11 @@ from .handler import Handler
 class Jump(Handler):
     header = ("JUMP", Color.GREEN, False)
     pattern = re.compile(
-        r"<(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}).\d{3}Z> \[Notice\] <Changing Solar System>.* Client entity ([\w-]*) .* changing system from ([\w-]*) to ([A-Za-z0-9]*) .*"
+        r"<(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}).\d{3}Z> \[Notice\] <Changing Solar System>.* Client entity ([\w-]*) .* changing system from (\w+) to (\w+) .*"
     )
 
     def format(self, data) -> str:
         whom = Color.GREEN(data[2])
         origin = Color.BLUE(data[3], bold=True)
         dest = Color.BLUE(data[4], bold=True)
-        return f"{whom} has departed {origin} for the {dest} system."
+        return f"{whom} has entered {dest} from the {origin} system."
