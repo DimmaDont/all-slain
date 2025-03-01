decad588f6d30f027558031ef4a2bff3f568d690
Dimma Don't <93484965+DimmaDont@users.noreply.github.com>
2025-02-28 20:41:11 -0500

    pass batch file arguments

--- run.bat
+++ run.bat
@@ -6,6 +6,6 @@ python -m venv venv --upgrade-deps >NUL 2>NUL
 echo downloading and installing dependencies...
 venv\Scripts\python.exe -m pip install -U -r requirements.txt >NUL 2>NUL
 
-venv\Scripts\python.exe allslain.py
+venv\Scripts\python.exe allslain.py %*
 
-pause
+exit /b
