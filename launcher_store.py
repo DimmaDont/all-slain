32f6bef3464ffa5bc74e060c386b5e417d035a52
Dimma Don't <93484965+DimmaDont@users.noreply.github.com>
2025-03-30 20:44:11 -0400

    add debug output to log finder

--- launcher_store.py
+++ launcher_store.py
@@ -1,5 +1,9 @@
 import json
+import logging
 import os
+
+
+logger = logging.getLogger("allslain").getChild("launcher_store")
 
 
 KEY = b"OjPs60LNS7LbbroAuPXDkwLRipgfH6hIFA6wvuBxkg4="
@@ -14,15 +18,17 @@ def read_launcher_store():
 
     try:
         with open(
-            f"{os.getenv('APPDATA')}\\rsilauncher\\launcher store.json", "rb"
+            Rf"{os.getenv('APPDATA')}\rsilauncher\launcher store.json", "rb"
         ) as f:
             encrypted_data = f.read()
 
         crypt = CryptSindresorhusConf(KEY, encrypted_data[:16])
         decrypted = crypt.decrypt(encrypted_data)
         return json.loads(decrypted)
-    except (OSError, json.JSONDecodeError):
-        raise LauncherStoreException("Failed to find or read launcher store") from None
+    except OSError:
+        raise LauncherStoreException("Failed to find launcher store") from None
+    except json.JSONDecodeError:
+        raise LauncherStoreException("Failed to read launcher store") from None
 
 
 def get_log() -> str | None:
@@ -38,6 +44,7 @@ def get_log() -> str | None:
 
         library_available = data["library"]["available"]
         if not library_available:
+            logger.debug("Nothing available")
             return None
 
         available_sc = [game for game in library_available if game["id"] == "SC"][0]
@@ -51,6 +58,7 @@ def get_log() -> str | None:
 
         library = data["library"]["installed"]
         if not library:
+            logger.debug("Nothing installed")
             # You don't have any games on your phone!
             return None
 
@@ -67,22 +75,27 @@ def get_log() -> str | None:
 
         files = {}
         for channel_id in available:
+            logger.debug("Available: %s", channel_id)
             # Just because it's available doesn't mean it's installed
             installed_channel = installed.get(channel_id)
             if not installed_channel:
                 continue
+            logger.debug("Installed: %s", channel_id)
 
             library_folder = installed_channel.get("libraryFolder")
             if not library_folder:
+                logger.debug("No library folder: %s", channel_id)
                 continue
             install_folder = installed_channel.get("installDir") or install_dirs.get(channel_id)  # fmt: skip
             if not install_folder:
+                logger.debug("No install folder: %s", channel_id)
                 continue
             file = f"{library_folder}{install_folder}\\{channel_id}\\Game.log"
             try:
                 files[file] = os.path.getmtime(file)
-            except OSError:
-                pass
+            except OSError as e:
+                logger.debug("%s %s %s", channel_id, file, str(e))
+
         return max(files, key=files.__getitem__) if files else None
     except (LookupError, ValueError) as e:
         raise LauncherStoreException(
