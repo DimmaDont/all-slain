a4e4afeefacb57e6fecc13f8245f5b3d43811387
Dimma Don't <93484965+DimmaDont@users.noreply.github.com>
2025-04-01 19:54:00 -0400

    fix drag drop from external folders

--- run.bat
+++ run.bat
@@ -1,11 +1,11 @@
 @echo off
 
 echo setting up...
-python -m venv venv --upgrade-deps >NUL 2>NUL
+python -m venv %~dp0\venv --upgrade-deps >NUL 2>NUL
 
 echo downloading and installing dependencies...
-venv\Scripts\python.exe -m pip install -U -r requirements.txt >NUL 2>NUL
+%~dp0\venv\Scripts\python.exe -m pip install -U -r %~dp0\requirements.txt >NUL 2>NUL
 
-venv\Scripts\python.exe allslain.py %*
+%~dp0\venv\Scripts\python.exe %~dp0\allslain.py %*
 
 exit /b
