187547a3ca6f7dcb8bc5b3daceb8010d0b44a3f1
Dimma Don't <93484965+DimmaDont@users.noreply.github.com>
2025-04-17 19:20:19 -0400

    fix actor stall regex

--- handlers/actor_stall.py
+++ handlers/actor_stall.py
@@ -9,7 +9,7 @@ from .handler import Handler
 class ActorStall(CompatibleAll, Handler):
     header = ("STALL", Color.WHITE, False)
     pattern = re.compile(
-        r"\[Notice\] \<Actor stall\> Actor stall detected, Player: ([\w-]+), Type: (?:up|down)stream, Length: (\d\.\d+)\. \[Team_ActorTech\]\[Actor\]"
+        r"\[Notice\] \<Actor stall\> Actor stall detected, Player: ([\w-]+), Type: (?:up|down)stream, Length: (\d+\.\d+)\. \[Team_ActorTech\]\[Actor\]"
     )
 
     def format(self, data):
