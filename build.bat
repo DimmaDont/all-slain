462fab0c3b41f1e2a0c654bb6e0a702f220c3d15
Dimma Don't <93484965+DimmaDont@users.noreply.github.com>
2025-06-07 19:23:59 -0400

    fix dependency update

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pyinstaller --clean --onefile main.py --name allslain --icon icon/ded.ico --noupx --debug noarchive --exclude-module pkg_resources
+%~dp0\venv\Scripts\pyinstaller.exe --clean --onefile main.py --name allslain --icon icon/ded.ico --noupx --debug noarchive --exclude-module pkg_resources
---
+++ update_build.bat
@@ -0,0 +1,1 @@
+%~dp0\venv\Scripts\python.exe -m pip install -U --upgrade-strategy eager -e .[build_app]
